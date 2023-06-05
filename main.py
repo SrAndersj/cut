c22621ec4b52305a19c4a5f12bd4a35e253703d7
PcAudio <byandersj@gmail.com>
2023-06-04 20:58:54 -0500

    2 second version

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
+import os
+import subprocess
 
 
 def cutVideo(input_filename):
+    # Obtiene el directorio de salida basado en el archivo de entrada
+    output_directory = os.path.dirname(input_filename)
 
+    # Obtiene el nombre base del archivo de salida sin la extensión
+    base_filename = os.path.splitext(os.path.basename(input_filename))[0]
 
-
-    import subprocess
-
-    # Define el nombre del archivo de entrada y el nombre base del archivo de salida
-
-    output_directory = "/home/srandersj/Vídeos/"
-    output_filename_base = output_directory + "recortado"
+    # Crea una carpeta con el nombre base del archivo de entrada en el directorio de salida
+    output_folder = os.path.join(output_directory, base_filename)
+    os.makedirs(output_folder, exist_ok=True)
 
     # Define la duración deseada de cada clip en segundos
     clip_duration = 30
 
-    command = f"ffmpeg -i {input_filename} -c copy -segment_time {clip_duration} -f segment {output_filename_base}%03d.mp4"
+    # Ejecuta el comando ffmpeg para recortar el video
+    command = f"ffmpeg -i {input_filename} -c copy -segment_time {clip_duration} -f segment {output_folder}/{base_filename}_%03d_recortado.mp4"
     subprocess.call(command, shell=True)
-
-    # Comando base de ffmpeg para recortar el video
-    # -i <archivo_de_entrada>: Especifica el archivo de video de entrada
-    # -c copy: Copia el flujo de video y audio sin realizar ninguna conversión
-    # -segment_time <duración>: Establece la duración deseada de cada segmento o clip
-    # -f segment: Indica el formato de salida de los segmentos
-    # {output_filename_base}%03d.mp4: Nombre base para los archivos de salida, seguido de números secuenciales de tres dígitos para nombrar los archivos
 
 
 if __name__ == '__main__':
     cutVideo("/home/srandersj/Vídeos/1videoTest.mp4")
-
-
